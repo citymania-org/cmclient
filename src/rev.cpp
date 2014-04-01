25cdbba40206110cc1e6e9f79616a9924dfd9701
Pavel Stupnikov <dp@dpointer.org>
2014-04-02 01:26:33 +0400

    1.4 rev.cpp

--- src/rev.cpp
+++ src/rev.cpp
@@ -37,7 +37,7 @@ bool IsReleasedVersion()
  * norev000 is for non-releases that are made on systems without
  * subversion or sources that are not a checkout of subversion.
  */
-const char _openttd_revision[] = "1.4.0-beta5";
+const char _openttd_revision[] = "1.4.0";
 
 /**
  * The text version of OpenTTD's build date.
@@ -70,11 +70,11 @@ const byte _openttd_revision_modified = 0;
  * final release will always have a lower version number than the released
  * version, thus making comparisons on specific revisions easy.
  */
-const uint32 _openttd_newgrf_version = 1 << 28 | 4 << 24 | 0 << 20 | 1 << 19 | (0 & ((1 << 19) - 1));
+const uint32 _openttd_newgrf_version = 1 << 28 | 4 << 24 | 0 << 20 | 1 << 19 | (26440 & ((1 << 19) - 1));
 
 #ifdef __MORPHOS__
 /**
  * Variable used by MorphOS to show the version.
  */
-extern const char morphos_versions_tag[] = "$VER: OpenTTD h7e3bd3a1M (02.04.14) OpenTTD Team [MorphOS, PowerPC]";
+extern const char morphos_versions_tag[] = "$VER: OpenTTD 1.4.0 (02.04.14) OpenTTD Team [MorphOS, PowerPC]";
 #endif
