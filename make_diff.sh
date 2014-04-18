d7da9fd1975f4524760db4b3dfaa989f881d7c98
Pavel Stupnikov <dp@dpointer.org>
2014-04-18 13:20:37 +0400

    more excludes from diff

--- make_diff.sh
+++ make_diff.sh
@@ -1,1 +1,1 @@
-hg diff -r openttd -X make_diff.sh -X novattd.sublime-project -X src/rev.cpp
+hg diff -r openttd -X make_diff.sh -X novattd.sublime-project -X src/rev.cpp -X .hgignore -X src/rev.cpp.in -X Makefile.src.in
