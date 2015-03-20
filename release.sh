2737dc1f75a24ac06905745decde97c6bf8cf657
Pavel Stupnikov <dp@dpointer.org>
2015-03-20 04:19:13 +0300

    updated make_diff, added check_diff
    --HG--
    branch : novattd150

--- release.sh
+++ release.sh
@@ -5,6 +5,14 @@ mkdir $TMP
 unzip -d $TMP release.zip
 cp -r bin/lang $TMP
 cp nova_changelog.txt $TMP/changelog.txt
-cp -r bin/lang $TMP
 rm $TMP/nova*.diff
 cp nova*.diff $TMP
+DIR=`pwd`
+pushd $TMP
+cp $DIR/bin/openttd.exe openttd.exe
+rm $DIR/novapolis_client.zip
+zip -9 -r $DIR/novapolis_client.zip *
+cp $DIR/bin/openttd64.exe openttd.exe
+rm $DIR/novapolis_client_64.zip
+zip -9 -r $DIR/novapolis_client_64.zip *
+popd
