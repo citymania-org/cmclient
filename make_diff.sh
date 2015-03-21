9779b8036838b74ffb55acd63da7408858fd75ea
Pavel Stupnikov <dp@dpointer.org>
2015-03-21 16:03:20 +0300

    exclude build_number.txt from diff

--- make_diff.sh
+++ make_diff.sh
@@ -1,1 +1,1 @@
-hg diff -r openttd -B --nodates -X make_diff.sh -X release.sh -X check_diff.sh -X novattd.sublime-project -X src/rev.cpp -X .hgignore -X src/rev.cpp.in -X Makefile.src.in
+hg diff -r openttd -B --nodates -X make_diff.sh -X release.sh -X check_diff.sh -X novattd.sublime-project -X src/rev.cpp -X .hgignore -X src/rev.cpp.in -X Makefile.src.in -X build_number.txt
