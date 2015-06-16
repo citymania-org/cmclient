d47d855e2b7ab085603b17068fd2f2b8bf5a766f
Pavel Stupnikov <dp@dpointer.org>
2015-06-16 13:31:03 +0300

    1.5.1-2 release; revoked rev.cpp inclusion, zip with hidden files instead; changelog updated

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 TMP="/tmp/cmclient"
 WIN="/var/run/media/pavels/F8040EC2040E843A/novattd"
 VER="1.5.1"
-CMVER="1.5.1"
+CMVER="1.5.1-2"
 TMP_SRC="$TMP/openttd-$VER"
 DIR=`pwd`
 SRC_RELEASE_FNAME="$DIR/citymania-client-$CMVER-source.zip"
@@ -12,11 +12,11 @@ rm $RELEASE_FNAME
 rm $RELEASE64_FNAME
 rm -rf $TMP
 mkdir $TMP
-hg diff -r openttd -B --nodates -X release_files -X make_diff.sh -X release.sh -X check_diff.sh -X novattd.sublime-project -X .hgignore -X src/rev.cpp.in -X Makefile.src.in -X build-number.txt > $TMP/cmclient.diff
+hg diff -r openttd -B --nodates -X src/rev.cpp -X release_files -X make_diff.sh -X release.sh -X check_diff.sh -X novattd.sublime-project -X .hgignore -X src/rev.cpp.in -X Makefile.src.in -X build-number.txt > $TMP/cmclient.diff
 tar xf ~/Downloads/openttd-$VER-source.tar.* -C $TMP
 patch -p1 -d $TMP_SRC < $TMP/cmclient.diff
 pushd $TMP_SRC
-zip -9 -r $SRC_RELEASE_FNAME *
+zip -9 -r $SRC_RELEASE_FNAME .
 ./configure
 make lang
 popd
@@ -27,7 +27,7 @@ cp -r $DIR/release_files/* $TMP/release/
 cp $DIR/cm_changelog.txt $TMP/release/citymania_changelog.txt
 pushd $TMP/release
 cp $WIN/bin/openttd.exe openttd.exe
-zip -9 -r $RELEASE_FNAME *
+zip -9 -r $RELEASE_FNAME .
 cp $WIN/bin/openttd64.exe openttd.exe
-zip -9 -r $RELEASE64_FNAME *
+zip -9 -r $RELEASE64_FNAME .
 popd
