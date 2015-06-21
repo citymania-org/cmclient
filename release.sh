42280d870d0a7c0106d13b0b58d3accd9853cc15
Pavel Stupnikov <dp@dpointer.org>
2015-06-21 17:58:17 +0300

    produce diff on release

--- release.sh
+++ release.sh
@@ -7,14 +7,15 @@ DIR=`pwd`
 SRC_RELEASE_FNAME="$DIR/citymania-client-$CMVER-source.zip"
 RELEASE_FNAME="$DIR/citymania-client-$CMVER-win32.zip"
 RELEASE64_FNAME="$DIR/citymania-client-$CMVER-win64.zip"
+RELEASE_DIFF="$DIR/citymania-client-$CMVER.diff"
 rm $SRC_RELEASE_FNAME
 rm $RELEASE_FNAME
 rm $RELEASE64_FNAME
 rm -rf $TMP
 mkdir $TMP
-hg diff -r openttd -B --nodates -X src/rev.cpp -X release_files -X make_diff.sh -X release.sh -X check_diff.sh -X novattd.sublime-project -X .hgignore -X src/rev.cpp.in -X Makefile.src.in -X build-number.txt > $TMP/cmclient.diff
+hg diff -r openttd -B --nodates -X src/rev.cpp -X release_files -X make_diff.sh -X release.sh -X check_diff.sh -X novattd.sublime-project -X .hgignore -X src/rev.cpp.in -X Makefile.src.in -X build-number.txt > $RELEASE_DIFF
 tar xf ~/Downloads/openttd-$VER-source.tar.* -C $TMP
-patch -p1 -d $TMP_SRC < $TMP/cmclient.diff
+patch -p1 -d $TMP_SRC < $RELEASE_DIFF
 pushd $TMP_SRC
 zip -9 -r $SRC_RELEASE_FNAME .
 ./configure
