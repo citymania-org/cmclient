7d694ddb1191d63ec6cbaf936e0c3b58ecb552ec
Pavel Stupnikov <dp@dpointer.org>
2017-04-18 13:44:39 +0300

    Bump version in release script

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 TMP="/tmp/cmclient"
 WIN="/var/run/media/pavels/F8040EC2040E843A/cmclient"
-VER="1.6.1"
-CMVER="1.6.1-2"
+VER="1.7.0"
+CMVER="1.7.0"
 TMP_SRC="$TMP/openttd-$VER"
 DIR=`pwd`
 SRC_RELEASE_FNAME="$DIR/citymania-client-$CMVER-source.zip"
