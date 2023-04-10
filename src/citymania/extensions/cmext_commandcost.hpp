b17a7e6782c807f8188dda0b9f0953b5bdf859fa
dP <dp@dpointer.org>
2023-04-11 01:12:27 +0400

    Add missing cmext_commandcost.hpp

---
+++ src/citymania/extensions/cmext_commandcost.hpp
@@ -0,0 +1,17 @@
+#ifndef CMEXT_COMMANDCOST_HPP
+#define CMEXT_COMMANDCOST_HPP
+
+namespace citymania {
+
+namespace ext {
+
+class CommandCost {
+public:
+    size_t industry_layout;
+};
+
+} // namespace ext
+
+} // namespace citymania
+
+#endif
