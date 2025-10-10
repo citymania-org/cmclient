e875986c78d168425e5ad72cab94ec9c320c3704
dP <dp@dpointer.org>
2025-10-10 20:35:37 +0500

    Fix crash when building airport

--- src/citymania/cm_station_gui.cpp
+++ src/citymania/cm_station_gui.cpp
@@ -1496,7 +1496,7 @@ up<Command> AirportBuildTool::SizedPlacementAction::GetCommand(TileIndex tile, S
 }
 
 bool AirportBuildTool::SizedPlacementAction::Execute(TileIndex tile) {
-    ExecuteBuildCommand(this, &CcBuildAirport, tile);
+    return ExecuteBuildCommand(this, &CcBuildAirport, tile);
 }
 
 std::optional<ObjectHighlight> AirportBuildTool::SizedPlacementAction::GetObjectHighlight(TileIndex tile) {
