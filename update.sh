0a4c2e3577a818b3aef08b88cb96aa1463277a39
dP <dp@dpointer.org>
2026-06-11 17:14:17 +0500

    Shange update.sh to git workflow (more of doc than actual script now)

--- update.sh
+++ update.sh
@@ -1,29 +1,7 @@
-version=$1
-dir=`mktemp -d`
-curdir=`pwd`
-echo $dir
-cd $dir
-wget https://cdn.openttd.org/openttd-releases/$version/openttd-$version-source.tar.xz
-tar xf openttd-$version-source.tar.xz
-ttdir=$dir/openttd-$version
-cp -rT $ttdir $curdir
-cd $ttdir
-find . -type f > $dir/ttd_files
-find * > $dir/ttd_all
-mkdir build
-cd build
-cmake ..
-make -j 4
-# cd objs/release
-# make $ttdir/src/rev.cpp
-cd $curdir
-cp $ttdir/build/generated/rev.cpp src/rev.cpp.in
-cat $dir/ttd_files | xargs git add
-for i in $(find * -not -path "build*" -not -path "update.sh"); do
-    if ! grep -qxFe "$i" $dir/ttd_all; then
-        echo "Deleting: $i"
-        # the next line is commented out.  Test it.  Then uncomment to removed the files
-        git rm -f "$i"
-    fi
-done
-# rm -rf $dir
+git fetch upstream
+git checkout upstream/master
+git rebase -i master --exec "sh rebase-check.sh"
+git checkout master
+# git merge <detached head>
+# possibly git reset --hard upstream/master
+# check git merge-base master upstream/master
